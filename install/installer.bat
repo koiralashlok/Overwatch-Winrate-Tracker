e106e4982d31ccf29b3b7e7e38af4a0c989012ba
Shlok <shlok_mahesh_koirala@sfu.ca>
2024-05-11 02:30:19 -0700

    Fixed functionality, corrected path and added some folder structure to install location

--- install/installer.bat
+++ install/installer.bat
@@ -3,24 +3,28 @@
 @REM Create a standalone executable file for Winrate Tracker
 @REM Path must be correct so that the db, etc can be accessed
 echo Installing Winrate Tracker...
-pyinstaller ..\src\winrate_tracker.py --distpath ..\ --workpath ..\build --specpath ..\app_spec --clean --name "Winrate Tracker" --log-level ERROR --onefile
+set "basepath=%~dp0..\Winrate Tracker"
+@REM TODO do away with the warning
+pyinstaller ..\src\winrate_tracker.py --distpath "%basepath%" --workpath ..\build --specpath ..\app_spec --clean --name "Winrate Tracker" --log-level ERROR --onefile
 
 @REM Create a db folder if it does not exist
 @echo off
 set "dbFolder=db"
-if not exist "%dbFolder%" (
-    mkdir "%dbFolder%"
-    > "winrate.csv"
+if not exist "%basepath%\%dbFolder%" (
+    mkdir "%basepath%\%dbFolder%"
+    echo map,wins,losses> "%basepath%\%dbFolder%\winrate.csv"
     echo Folder "%dbFolder%" created.
 ) else (
     echo Folder "%dbFolder%" already exists.
 )
 
+@REM Add the etl columns (so that we can remove)
+python ..\src\etl\perform_etl.py
+
 @REM Developer Mode so don't delete build and app_spec folders
-if "%1"!="true" (
-    rd /S /Q  ..\build
-    rd /S /Q ..\app_spec
+if not "%1"=="true" (
+    rd /S /Q %~dp0..\build
+    rd /S /Q %~dp0..\app_spec
 )
 
-echo Press Enter to exit...
 PAUSE
