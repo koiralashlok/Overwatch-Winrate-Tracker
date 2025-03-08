57c5c2c1a0c54a9e959c0a249b68212734583f1d
Shlok <106499763+koiralashlok@users.noreply.github.com>
2025-03-07 17:01:42 -0800

    updated settings to allow CORS

--- backend/winrate_tracker_server/settings.py
+++ backend/winrate_tracker_server/settings.py
@@ -31,15 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -48,6 +51,12 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+# CORS configs
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Allow requests from React frontend
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "winrate_tracker_server.urls"
 
