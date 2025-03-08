7c26739f645ab170732e958cc81c3a9ea87f412b
Shlok <106499763+koiralashlok@users.noreply.github.com>
2025-03-07 17:02:16 -0800

    reach the be endpoint from fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [msg, setMsg] = useState("");
+    const [myTs, setTs] = useState("")
+
+    useEffect(() => {
+      fetch("http://127.0.0.1:8000/tracker/ping_frontend")
+          .then((res) => res.json())
+          .then(({ message, ts }) => {
+              setMsg(message);  // Use message
+              setTs(ts);   // Use ts (or set it in state if needed)
+          });
+  }, []);
+  
+    return <>
+      <h1>The response at {myTs} was:</h1>
+      <h1>{msg}</h1>
+    </>;
 }
 
 export default App;
