f53034e84ed92539e302d9573afecd1ed823d270
Shlok <106499763+koiralashlok@users.noreply.github.com>
2025-03-07 16:19:47 -0800

    added frontend boilerplate and updated gitignore accordingly

--- backend/winrate_tracker_app/views.py
+++ backend/winrate_tracker_app/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from winrate_tracker_app.src import dummy as wt
+from django.http import JsonResponse
+import datetime
+
+def ping_frontend(request):
+    return JsonResponse({
+        "ts": f"{datetime.datetime.now()}",
+        "message": "Hello from Django at"
+    })
 
 def index(request):
     return HttpResponse("Hello, world. You're at the TRACKER PAGE.")
