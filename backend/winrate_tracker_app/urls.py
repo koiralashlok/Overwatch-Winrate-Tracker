cb7dcba35b940f02ed927480e414f7f8e02464d9
Shlok <106499763+koiralashlok@users.noreply.github.com>
2025-03-09 00:33:38 -0800

    expose get db endpoint

--- backend/winrate_tracker_app/urls.py
+++ backend/winrate_tracker_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # can use regex too for optional parameters or what not
     path("ping_frontend/<int:id>/", views.ping_frontend, name="ping_frontend"),
     path("test/", views.someView, name="whatdoesthisnamedo"),
+    # actual endpoints
+    path("get_winrate_data_by_id/<str:id>/", views.get_winrate_data_by_id, name="get_winrate_data_by_id"),
 ]
