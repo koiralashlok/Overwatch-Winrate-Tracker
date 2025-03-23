fd65d0cbb4d383fdcd93773eb3bea3f6977c805d
Shlok <smk22@sfu.ca>
2025-03-23 00:45:43 -0700

    new script to create env variables

---
+++ backend/create_env_files.py
@@ -0,0 +1,23 @@
+import json
+import os
+from pathlib import Path
+from aws_utils import AWSUtils
+
+# If secrets exist, don't go to AWS param store
+BASE_DIR = Path(__file__).resolve().parent
+secrets_file = BASE_DIR / 'backend-secrets.json'
+if not os.path.isfile(secrets_file):
+    utils = AWSUtils()
+    ec2_ip = utils.get_ssm_parameter('/ow_winrate_tracker/backend_url')
+else:
+    with open(secrets_file, 'r') as file:
+        data = json.load(file)
+        ec2_ip = data['EC2_IP']
+
+# Write to env file and load it
+with open(BASE_DIR / '.env', 'w') as be_env:
+    be_env.writelines(f'EC2_IP={ec2_ip}')
+
+# Create env file for FE
+with open(BASE_DIR.parent / 'frontend' / '.env', 'w') as fe_env:
+    fe_env.writelines(f'REACT_APP_BACKEND_URL=http://{ec2_ip}:8000/tracker')
