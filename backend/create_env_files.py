2ca5706385e40efc4e199a83acf24d1e9a8ecd9f
Shlok <smk22@sfu.ca>
2025-04-01 18:11:27 -0700

    added db key and debug mode to env files from aws kms

--- backend/create_env_files.py
+++ backend/create_env_files.py
@@ -9,14 +9,20 @@ secrets_file = BASE_DIR / 'backend-secrets.json'
 if not os.path.isfile(secrets_file):
     utils = AWSUtils()
     ec2_ip = utils.get_ssm_parameter('/ow_winrate_tracker/backend_url')
+    db_key = utils.get_ssm_parameter('/db/secret-key')
+    debug_mode = utils.get_ssm_parameter('ow_winrate_tracker/debug_mode')
 else:
     with open(secrets_file, 'r') as file:
         data = json.load(file)
         ec2_ip = data['EC2_IP']
+        db_key = data['DB_SECRET_KEY']
+        debug_mode = data['DEBUG_MODE']
 
 # Write to env file and load it
 with open(BASE_DIR / '.env', 'w') as be_env:
     be_env.writelines(f'EC2_IP={ec2_ip}')
+    be_env.writelines(f'DB_SECRET_KEY={db_key}')
+    be_env.writelines(f'DEBUG_MODE={debug_mode}')
 
 # Create env file for FE
 with open(BASE_DIR.parent / 'frontend' / '.env', 'w') as fe_env:
